5e3f8e485c44d2d60f1eb1f87db31779bbe292db
Arun Kumar Gupta <arunkrguptaa@gmail.com>
2020-07-26 07:02:22 +0530

    remove divider

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles.css";
 import Articles from "./Articles";
+import LineChart from "./LineChart";
 
 export default function App() {
   return (
     <div>
       <Articles />
+      <LineChart />
     </div>
   );
 }
